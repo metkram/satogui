067fcf11226b51f04a106784ddc6a4ec4e234188
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2023-09-07 13:39:06 -0400

    feat: initial commit

---
+++ src/components/QrCode.svelte
@@ -0,0 +1,87 @@
+<script lang="ts">
+	import QRCode from 'qrcode';
+	import Button from '$components/Button.svelte';
+	import { onDestroy, onMount } from 'svelte';
+
+	const MAX_CHECK_ATTEMPTS = 10;
+
+	let paymentHash: string;
+	let checkAttempts = 0;
+	let error = '';
+	let polling = false;
+	let copyInterval: NodeJS.Timeout;
+	let pollInterval: NodeJS.Timeout;
+
+	export let invoice: string = '';
+	export let paid = false;
+
+	async function fetchInvoice() {
+		try {
+			const result = await fetch('/api/invoice');
+			const { r_hash, payment_request } = await result.json();
+			invoice = payment_request;
+			paymentHash = r_hash;
+		} catch (e) {
+			console.error(e);
+			alert(e);
+		}
+	}
+
+	function createQRCode() {
+		const canvas = document.getElementById('canvas');
+		QRCode.toCanvas(canvas, invoice as string, (error: unknown) => {
+			if (error) console.error(error);
+		});
+		polling = true;
+	}
+
+	async function pollForPayment() {
+		if (paymentHash) {
+			if (checkAttempts < MAX_CHECK_ATTEMPTS) {
+				const result = await fetch('/api/checkpayment', {
+					method: 'POST',
+					headers: { 'content-type': 'application/json' },
+					body: JSON.stringify({ paymentHash })
+				});
+				const { settled } = await result.json();
+				if (settled) {
+					invoice = '';
+					paid = true;
+				} else {
+					pollInterval = setTimeout(pollForPayment, 3000);
+					checkAttempts++;
+				}
+			} else if (checkAttempts > MAX_CHECK_ATTEMPTS) {
+				checkAttempts = 0;
+				error = 'Timeout, too many checks';
+				polling = false;
+				invoice = '';
+			}
+		}
+	}
+
+	function copy() {
+		navigator.clipboard.writeText(invoice || '');
+		copyInterval = setTimeout(() => {}, 4000);
+	}
+
+	onDestroy(() => clearTimeout(pollInterval));
+
+	onMount(fetchInvoice);
+	$: invoice && createQRCode();
+	$: polling && !paid && pollForPayment();
+</script>
+
+<div class="flex flex-col gap-4 items-center">
+	{#if !paid}
+		<canvas id="canvas" />
+	{/if}
+	{#if error}
+		<p>{error}</p>
+	{/if}
+	{#if !invoice && !paid}
+		<p>Loading...</p>
+	{:else if !paid}
+		<Button on:click={copy}>Copy Invoice</Button>
+	{/if}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<h1>Satogram</h1>
+
+<form>
+	<label for="budget">Total Budget</label>
+	<input type="text" placeholder="" name="budget" />
+	<label for="amount">Amount Per Satogram</label>
+	<input type="text" placeholder="" name="amount" />
+	<label for="fees">Max Fees</label>
+	<input type="text" placeholder="" name="fees" />
+	<label for="message">Message</label>
+	<input type="text" placeholder="" name="message" />
+</form>
+
+<style lang="postcss">
+	/* An input that has a border, highlighted state, rounded, with some other good defaults */
+	input {
+		border: 1px solid #ccc;
+		border-radius: 4px;
+		padding: 0.5rem;
+		font-size: 1rem;
+		transition: border-color 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
+	}
+</style>
